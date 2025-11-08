49835cf50f9c8a44c83c5a752c0d6ade55208c94
ryanp03200 <ryanporter0320@gmail.com>
2025-11-08 12:19:38 -0500

    11/8/25

--- Assets/Pixel_Quest/Scripts/Player Stats.cs
+++ Assets/Pixel_Quest/Scripts/Player Stats.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
+    private int coincounter = 0;
     private readonly string nextlevel;
-
+    private int _health = 3;
+    private int _maxhealth = 3;
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -24,7 +26,21 @@ public class PlayerStats : MonoBehaviour
                 {
                     SceneManager.LoadScene(nextlevel);
                     break;
+         
                 }
+            case "Coin":
+                
+                { 
+                        coincounter++;
+                  Destroy(collision.gameObject); 
+                    break;
+                }
+            case "Health":
+                { _health++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+               
         }
     }
 }
